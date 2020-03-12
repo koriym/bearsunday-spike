a1330ed7cacb371bba5041e991613eaf7b273308
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-03-12 15:54:09 +0900

    initial commit

---
+++ src/Module/AppModule.php
@@ -0,0 +1,21 @@
+<?php
+namespace MyVendor\MyProject\Module;
+
+use BEAR\Package\AbstractAppModule;
+use BEAR\Package\PackageModule;
+use josegonzalez\Dotenv\Loader;
+
+class AppModule extends AbstractAppModule
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure() : void
+    {
+        $env = dirname(__DIR__) . '/.env';
+        if (file_exists($env)) {
+            (new Loader($env))->parse()->putenv(true);
+        }
+        $this->install(new PackageModule);
+    }
+}
